6ac378d389564bbc34670d615d83835df42edbc1
aksoy <aksoy.android@gmail.com>
2020-03-10 21:14:55 +0300

    Add project files.

---
+++ Halı Saha Yönetim/PageScreens/ReservationAttempt.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Halı_Saha_Yönetim.PageScreens
+{
+    /// <summary>
+    /// Interaction logic for ReservationAttempt.xaml
+    /// </summary>
+    public partial class ReservationAttempt : Page
+    {
+        public ReservationAttempt()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSaveResAttempt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Halı Saha Yönetim/PageScreens/ReservationShow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Halı_Saha_Yönetim
+{
+    /// <summary>
+    /// Interaction logic for ReservationShow.xaml
+    /// </summary>
+    public partial class ReservationShow : Page
+    {
+        public ReservationShow()
+        {
+            InitializeComponent();
+        }
+    }
+}
